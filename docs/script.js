b7f94adcc0e636581321e52c90d7231237c77e47
Jamie Burnett <JamierBurnett@gmail.com>
2024-11-14 17:57:36 +0000

    Update script.js
    Carousel logo link made to be relative. To see if it works on Github... to display the logos when using theGit link to website.

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/logos')
+    const isLocal = window.location.hostname === 'localhost';
+    const logosPath = isLocal ? '/logos' : 'CompanyLogos';
+
+    fetch(logosPath)
         .then(response => response.json())
         .then(logos => {
             const carouselInner = document.getElementById('carousel-inner');
             logos.forEach(logo => {
                 const img = document.createElement('img');
-                img.src = `CompanyLogos/${logo}`;
+                img.src = `${isLocal ? 'CompanyLogos/' : ''}${logo}`;
                 img.alt = logo;
                 carouselInner.appendChild(img);
             });
@@ -13,13 +16,15 @@ document.addEventListener('DOMContentLoaded', () => {
             // Clone the logos to create an infinite loop effect
             logos.forEach(logo => {
                 const img = document.createElement('img');
-                img.src = `CompanyLogos/${logo}`;
+                img.src = `${isLocal ? 'CompanyLogos/' : ''}${logo}`;
                 img.alt = logo;
                 carouselInner.appendChild(img);
             });
         })
         .catch(error => console.error('Error fetching logos:', error));
 });
+
+
 
 
 
